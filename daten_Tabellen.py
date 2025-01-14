aaa36464aefc2719efd0d90343797bcb93407bf3
Paul Franzen <paulfra345@gmail.com>
2025-01-14 16:41:42 +0100

    Jahreszahlen_new_brNCH_(daten_Tabellen)

--- daten_Tabellen.py
+++ daten_Tabellen.py
@@ -1,6 +1,7 @@
 ##Programm um Box-Whisker- unde Scatterplot für gew Datei macht
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 def plots(filename,xlable,ylable,title,box=False,scatter=False):
     #einlesen
@@ -11,24 +12,38 @@ def plots(filename,xlable,ylable,title,box=False,scatter=False):
         df.boxplot(column=[ylable])
 
     #Titel und Achsenbeschriftungen
-        plt.title('Boxplot aus'+title+'.csv')
+        plt.title('Boxplot aus '+title+'.csv')
         plt.ylabel(ylable)
+        plt.xticks([])
 
     # Diagramm anzeigen
         plt.show()
 
-
     # Scatterplot erstellen
-    if scatter == True:
-        plt.scatter(df[xlable],df[ylable],color='blue',alpha=0.7,s=5)
+    df[xlable] = pd.to_datetime(df[xlable], format='%d.%m.%Y')
 
-    # Titel und Achsenbeschriftungen für den Scatterplot hinzufügen
-        plt.title('Scatterplot aus'+title+'.csv')
+    if scatter:
+        plt.figure(figsize=(10, 6))
+        plt.scatter(df[xlable], df[ylable], color='blue', alpha=0.7, s=5)
+
+        # Titel und Achsenbeschriftungen für den Scatterplot hinzufügen
+        plt.title('Scatterplot aus ' + title + '.csv')
         plt.xlabel(xlable)
         plt.ylabel(ylable)
 
-    # Diagramm anzeigen
+        # Achseneinteilung so einstellen, dass nur jedes Jahrzehnt angezeigt wird
+        #locator = mdates.YearLocator(1)  # Locator für jedes Jahrzehnt
+        #formatter = mdates.DateFormatter('%Y')  # Formatter für das Jahr
+
+        #ax = plt.gca()
+        #ax.xaxis.set_major_locator(locator)
+        #ax.xaxis.set_major_formatter(formatter)
+
+        # Optional: Automatische Drehung der Datumsbeschriftungen für bessere Lesbarkeit
+        #plt.gcf().autofmt_xdate()
+
+        # Diagramm anzeigen
         plt.show()
     return 0
 
-plots('data-1.csv','DATE','Percent change from a year ago','Test',False,True)
+plots('data-2.csv','DATE','Percent change from a year ago','data-1',False,True)
